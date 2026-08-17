4b1f5cf547f1878cb12f38bff294d0a3fab38126
Charly-SCA <charlychipahua@gmail.com>
2026-08-17 17:59:08 -0600

    Add files via upload
    Tema HelloWorld de kotlin

---
+++ HelloWorld.kt
@@ -0,0 +1,10 @@
+// Tema 1: Hello World
+// Ejercicio: Completar el codigo para que imprima "Mary is 20 years old"
+// Carlos Santiago Chipahua Alvarez
+
+fun main() {
+    val name = "Mary"
+    val age = 20
+    // Write your code here
+    println("$name is $age years old")
+}
