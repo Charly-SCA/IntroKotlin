2597d4953e2cf091a8053d5442a65898ccb811ea
Charly-SCA <charlychipahua@gmail.com>
2026-08-17 18:00:42 -0600

    Tema 2 de BasicTypes de kotlin

---
+++ BasicTypes.kt
@@ -0,0 +1,12 @@
+// Tema 2: Basic Types
+// Ejercicio: Declarar explicitamente el tipo correcto para cada variable
+// Carlos Santiago Chipahua Alvarez
+
+fun main() {
+    val a: Int = 1000
+    val b: String = "log message"
+    val c: Double = 3.14
+    val d: Long = 100_000_000_000_000
+    val e: Boolean = false
+    val f: Char = '\n'
+}
